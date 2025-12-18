7af3ceddb67d46b02b550a3f33494ef0ff06b5e2
unknown <kaustuv.duwadi@gmail.com>
2025-12-18 13:22:40 +0545

    Initial commit

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PersonalJournalApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
