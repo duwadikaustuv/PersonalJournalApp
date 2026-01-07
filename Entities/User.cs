956eb798fb59f8cdeeac755e5a0be4f00f9b2729
unknown <kaustuv.duwadi@gmail.com>
2026-01-07 21:56:42 +0545

    feat: Update project structure and add user entity

---
+++ Entities/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace PersonalJournalApp.Entities
+{
+    public class User : IdentityUser
+    {
+        // Navigation properties
+        public ICollection<JournalEntry> JournalEntries { get; set; } = new List<JournalEntry>();
+        public ICollection<Tag> Tags { get; set; } = new List<Tag>();
+        public ICollection<Category> Categories { get; set; } = new List<Category>();
+        public UserSettings? UserSettings { get; set; }
+        
+        // Additional user properties
+        public string? PIN { get; set; } // For app lock feature
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
--- obj/Debug/net9.0-ios/iossimulator-x64/PersonalJournalApp.AssemblyInfo.cs
+++ obj/Debug/net9.0-ios/iossimulator-x64/PersonalJournalApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PersonalJournalApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+672f8e32d43a442bffc04e5fa8b079642acd94af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+2f1777cfc92db1886bf3cf061940225556df4de3")]
 [assembly: System.Reflection.AssemblyProductAttribute("PersonalJournalApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PersonalJournalApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
